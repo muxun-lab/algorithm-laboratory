696f319e25d012873bece8f8ee2498f060be008f
SunshineDreams <mysunshinedreams@163.com>
2020-02-14 13:29:32 +0800

    [feature]: 基础算法——贪心算法

---
+++ algorithm-basic/src/main/java/com/sunshine/algorithm/greedy/GreedyActivitySelector.java
@@ -0,0 +1,15 @@
+package com.sunshine.algorithm.greedy;
+
+/**
+ * @author sunshine
+ * @created 2020-02-14
+ */
+public class GreedyActivitySelector {
+
+	/**
+	 *
+	 */
+	public void greedyActivitySelector() {
+
+	}
+}
---
+++ algorithm-basic/src/main/java/com/sunshine/algorithm/greedy/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * 基础算法——贪心算法
+ * @author sunshine
+ * @created 2020-02-14
+ */
+package com.sunshine.algorithm.greedy;
