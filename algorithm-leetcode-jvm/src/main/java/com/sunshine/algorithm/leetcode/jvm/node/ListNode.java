3c37964ac039aa48335c44fb93590b0ad5f5eb46
SunshineDreams <mysunshinedreams@163.com>
2020-02-14 22:32:04 +0800

    [feature]: Leetcode 2，两数相加

---
+++ algorithm-leetcode-jvm/src/main/java/com/sunshine/algorithm/leetcode/jvm/node/ListNode.java
@@ -0,0 +1,16 @@
+package com.sunshine.algorithm.leetcode.jvm.node;
+
+/**
+ * @author sunshine
+ * @created 2020-02-14
+ */
+public class ListNode {
+
+	int val;
+
+	ListNode next;
+
+	ListNode(int x) {
+		this.val = x;
+	}
+}
