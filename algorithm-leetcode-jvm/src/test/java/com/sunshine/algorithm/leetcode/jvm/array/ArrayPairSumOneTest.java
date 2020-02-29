eda40add5bb3e6ad08f2e43fb5d3b6fb29c84afd
SunshineDreams <mysunshinedreams@163.com>
2020-02-29 19:17:49 +0800

    [feature]: LeetCode 561, 数组拆分 I

---
+++ algorithm-leetcode-jvm/src/test/java/com/sunshine/algorithm/leetcode/jvm/array/ArrayPairSumOneTest.java
@@ -0,0 +1,18 @@
+package com.sunshine.algorithm.leetcode.jvm.array;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author sunshine
+ * @created 2020-02-29
+ */
+@DisplayName("数组拆分 I测试类")
+public class ArrayPairSumOneTest {
+
+	@Test
+	public void arrayPairSumOneTestOne() {
+		Assertions.assertEquals(4, new ArrayPairSumOne().arrayPairSum(new int[]{1, 4, 3, 2}));
+	}
+}
