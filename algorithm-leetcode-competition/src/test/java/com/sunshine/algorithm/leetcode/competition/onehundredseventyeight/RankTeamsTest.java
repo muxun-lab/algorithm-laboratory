8a116cd912c59e9455d3432eef18b30ff49bde21
SunshineDreams <mysunshinedreams@163.com>
2020-03-01 12:43:54 +0800

    [feature]: LeetCode 第178场周赛

---
+++ algorithm-leetcode-competition/src/test/java/com/sunshine/algorithm/leetcode/competition/onehundredseventyeight/RankTeamsTest.java
@@ -0,0 +1,28 @@
+package com.sunshine.algorithm.leetcode.competition.onehundredseventyeight;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author sunshine
+ * @created 2020-03-01
+ */
+@DisplayName("通过投票对团队排名测试类")
+public class RankTeamsTest {
+
+	@Test
+	public void rankTeamsTestOne() {
+		Assertions.assertEquals("ACB", new RankTeams().rankTeams(new String[]{"ABC", "ACB", "ABC", "ACB", "ACB"}));
+	}
+
+	@Test
+	public void rankTeamsTestTwo() {
+		Assertions.assertEquals("XWYZ", new RankTeams().rankTeams(new String[]{"WXYZ", "XYZW"}));
+	}
+
+	@Test
+	public void rankTeamsTestThree() {
+		Assertions.assertEquals("ABC", new RankTeams().rankTeams(new String[]{"BCA", "CAB", "CBA", "ABC", "ACB", "BAC"}));
+	}
+}
