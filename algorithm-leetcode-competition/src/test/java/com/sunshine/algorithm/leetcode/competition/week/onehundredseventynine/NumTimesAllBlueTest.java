06268ce35692989c645c6e3435cac85104cfd0df
SundialDreams <mysunshinedreams@163.com>
2020-03-08 20:56:48 +0800

    [feature]: LeetCode周赛, 第179场

---
+++ algorithm-leetcode-competition/src/test/java/com/sunshine/algorithm/leetcode/competition/week/onehundredseventynine/NumTimesAllBlueTest.java
@@ -0,0 +1,23 @@
+package com.sunshine.algorithm.leetcode.competition.week.onehundredseventynine;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author sunshine
+ * @created 2020-03-08
+ */
+@DisplayName("")
+public class NumTimesAllBlueTest {
+
+	@Test
+	public void numTimesAllBlueTestOne() {
+		Assertions.assertEquals(3, new NumTimesAllBlue().numTimesAllBlue(new int[]{2, 1, 3, 5, 4}));
+	}
+
+	@Test
+	public void numTimesAllBlueTestTwo() {
+		Assertions.assertEquals(1, new NumTimesAllBlue().numTimesAllBlue(new int[]{4, 1, 2, 3}));
+	}
+}
