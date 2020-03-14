a76fd662b7068934388c13e0830057930bca9e51
SundialDreams <mysunshinedreams@163.com>
2020-03-14 12:39:35 +0800

    [feature]: LeetCode 94, 二叉树的中序遍历

---
+++ algorithm-leetcode-jvm/src/main/java/com/sunshine/algorithm/leetcode/jvm/binarytree/BinaryTreeInorderTraversal.java
@@ -0,0 +1,118 @@
+package com.sunshine.algorithm.leetcode.jvm.binarytree;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * 题号：94
+ * 题目：二叉树的中序遍历
+ * 详情：给定一个二叉树，返回它的中序 遍历。
+ * @author sunshine
+ * @created 2020-03-13
+ */
+public class BinaryTreeInorderTraversal {
+
+	private List<Integer> resultList = new ArrayList<>();
+
+	/**
+	 * 思路：迭代
+	 * @param root 二叉树根节点
+	 * @return 二叉树的中序遍历
+	 */
+	public List<Integer> inorderTraversal(TreeNode root) {
+		if (root == null) {
+			return resultList;
+		}
+		LinkedList<TreeNode> leftStack = new LinkedList<>();
+		LinkedList<TreeNode> rightStack = new LinkedList<>();
+		LinkedList<TreeNode> centerStack = new LinkedList<>();
+		if (root.left != null) {
+			leftStack.push(root.left);
+		}
+		if (root.right != null) {
+			rightStack.push(root.right);
+		}
+		centerStack.push(root);
+		do {
+			TreeNode leftNode = null;
+			if (!leftStack.isEmpty()) {
+				leftNode = leftStack.pop();
+			}
+			if (hasLeaf(leftNode)) {
+				centerStack.push(leftNode);
+				leftStack.push(leftNode.left);
+				rightStack.push(leftNode.right);
+				continue;
+			} else if (leftNode != null) {
+				resultList.add(leftNode.val);
+			}
+			if (!centerStack.isEmpty()) {
+				TreeNode centerNode = centerStack.pop();
+				resultList.add(centerNode.val);
+			}
+			TreeNode rightNode = null;
+			if (!rightStack.isEmpty()) {
+				rightNode = rightStack.pop();
+			}
+			if (hasLeaf(rightNode)) {
+				centerStack.push(rightNode);
+				leftStack.push(rightNode.left);
+				rightStack.push(rightNode.right);
+			} else if (rightNode != null) {
+				resultList.add(rightNode.val);
+			}
+		} while (!leftStack.isEmpty() || !centerStack.isEmpty() || !rightStack.isEmpty());
+		return resultList;
+	}
+
+	private boolean hasLeaf(TreeNode currentNode) {
+		if (currentNode == null) {
+			return false;
+		}
+		return currentNode.left != null || currentNode.right != null;
+	}
+
+	/**
+	 * 思路：迭代升级
+	 * @param root 二叉树根节点
+	 * @return 二叉树的中序遍历
+	 */
+	public List<Integer> inorderTraversalIteratorUpgrade(TreeNode root) {
+		Stack<TreeNode> stack = new Stack<>();
+		TreeNode currentNode = root;
+		while (currentNode != null || !stack.isEmpty()) {
+			while (currentNode != null) {
+				stack.push(currentNode);
+				currentNode = currentNode.left;
+			}
+			currentNode = stack.pop();
+			resultList.add(currentNode.val);
+			currentNode = currentNode.right;
+		}
+		return resultList;
+	}
+
+	/**
+	 * 思路：递归
+	 * @param root 二叉树根节点
+	 * @return 二叉树的中序遍历
+	 */
+	public List<Integer> inorderTraversalRecursion(TreeNode root) {
+		if (root == null) {
+			return resultList;
+		}
+		recursion(root);
+		return resultList;
+	}
+
+	private void recursion(TreeNode currentNode) {
+		if (currentNode == null) {
+			return;
+		}
+		recursion(currentNode.left);
+		resultList.add(currentNode.val);
+		recursion(currentNode.right);
+	}
+}
