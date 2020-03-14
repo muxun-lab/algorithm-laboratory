4e098bddcc95880159556e215d595de8e96e423b
SundialDreams <mysunshinedreams@163.com>
2020-03-14 22:45:05 +0800

    [feature]: LeetCode 746, 使用最小花费爬楼梯

---
+++ algorithm-leetcode-jvm/src/test/java/com/sunshine/algorithm/leetcode/jvm/dp/MinCostClimbingStairsTest.java
@@ -0,0 +1,23 @@
+package com.sunshine.algorithm.leetcode.jvm.dp;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author sunshine
+ * @created 2020-03-14
+ */
+@DisplayName("使用最小花费爬楼梯单元测试")
+public class MinCostClimbingStairsTest {
+
+	@Test
+	public void minCostClimbingStairsTestOne() {
+		Assertions.assertEquals(15, new MinCostClimbingStairs().minCostClimbingStairs(new int[]{10, 15, 20}));
+	}
+
+	@Test
+	public void minCostClimbingStairsTestTwo() {
+		Assertions.assertEquals(6, new MinCostClimbingStairs().minCostClimbingStairs(new int[]{1, 100, 1, 1, 1, 100, 1, 1, 100, 1}));
+	}
+}
