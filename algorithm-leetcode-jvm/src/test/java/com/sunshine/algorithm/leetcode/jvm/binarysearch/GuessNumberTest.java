86f864446cccb4cdf2d60ec9b2792ce1cb2c031c
SundialDreams <mysunshinedreams@163.com>
2020-03-16 23:38:55 +0800

    [feature]: LeetCode 374, 猜数字大小

---
+++ algorithm-leetcode-jvm/src/test/java/com/sunshine/algorithm/leetcode/jvm/binarysearch/GuessNumberTest.java
@@ -0,0 +1,23 @@
+package com.sunshine.algorithm.leetcode.jvm.binarysearch;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author sunshine
+ * @created 2020-03-16
+ */
+@DisplayName("猜数字大小")
+public class GuessNumberTest {
+
+	@Test
+	public void guessNumberTestOne() {
+		Assertions.assertEquals(6, new GuessNumber(6).guessNumber(10));
+	}
+
+	@Test
+	public void guessNumberTestTwo() {
+		Assertions.assertEquals(15, new GuessNumber(382).guessNumber(15));
+	}
+}
