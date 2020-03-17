5ace4ede01a90dae69607e7fdfcea53d804bea1d
SundialDreams <mysunshinedreams@163.com>
2020-03-17 17:13:53 +0800

    [feature]: LeetCode 1025, 除数博弈

---
+++ algorithm-leetcode-jvm/src/test/java/com/sunshine/algorithm/leetcode/jvm/dp/DivisorGameTest.java
@@ -0,0 +1,23 @@
+package com.sunshine.algorithm.leetcode.jvm.dp;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author sunshine
+ * @created 2020-03-17
+ */
+@DisplayName("除数博弈单元测试")
+public class DivisorGameTest {
+
+	@Test
+	public void divisorGameTestOne() {
+		Assertions.assertTrue(new DivisorGame().divisorGame(2));
+	}
+
+	@Test
+	public void divisorGameTestTwo() {
+		Assertions.assertFalse(new DivisorGame().divisorGame(3));
+	}
+}
