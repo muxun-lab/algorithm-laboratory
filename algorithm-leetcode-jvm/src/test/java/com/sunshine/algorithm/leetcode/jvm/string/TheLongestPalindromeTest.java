1be47f686b37d58e9a24bbd7be4b6bead360fb7f
SundialDreams <mysunshinedreams@163.com>
2020-03-19 12:53:36 +0800

    [feature]: LeetCode 409, 最长回文字符串

---
+++ algorithm-leetcode-jvm/src/test/java/com/sunshine/algorithm/leetcode/jvm/string/TheLongestPalindromeTest.java
@@ -0,0 +1,23 @@
+package com.sunshine.algorithm.leetcode.jvm.string;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author sunshine
+ * @created 2020-03-19
+ */
+@DisplayName("最长回文字符串单元测试")
+public class TheLongestPalindromeTest {
+
+	@Test
+	public void longestPalindromeTestOne() {
+		Assertions.assertEquals(7, new TheLongestPalindrome().longestPalindrome("abccccdd"));
+	}
+
+	@Test
+	public void longestPalindromeTestTwo() {
+		Assertions.assertEquals(983, new TheLongestPalindrome().longestPalindrome("civilwartestingwhetherthatnaptionoranynartionsoconceivedandsodedicatedcanlongendureWeareqmetonagreatbattlefiemldoftzhatwarWehavecometodedicpateaportionofthatfieldasafinalrestingplaceforthosewhoheregavetheirlivesthatthatnationmightliveItisaltogetherfangandproperthatweshoulddothisButinalargersensewecannotdedicatewecannotconsecratewecannothallowthisgroundThebravelmenlivinganddeadwhostruggledherehaveconsecrateditfaraboveourpoorponwertoaddordetractTgheworldadswfilllittlenotlenorlongrememberwhatwesayherebutitcanneverforgetwhattheydidhereItisforusthelivingrathertobededicatedheretotheulnfinishedworkwhichtheywhofoughtherehavethusfarsonoblyadvancedItisratherforustobeherededicatedtothegreattdafskremainingbeforeusthatfromthesehonoreddeadwetakeincreaseddevotiontothatcauseforwhichtheygavethelastpfullmeasureofdevotionthatweherehighlyresolvethatthesedeadshallnothavediedinvainthatthisnationunsderGodshallhaveanewbirthoffreedomandthatgovernmentofthepeoplebythepeopleforthepeopleshallnotperishfromtheearth"));
+	}
+}
