71f2d327619c0d6103f61c5a2a7643a07a536ce1
SundialDreams <mysunshinedreams@163.com>
2020-03-22 16:01:21 +0800

    [feature]: LeetCode周赛, 第181场

---
+++ algorithm-leetcode-competition/src/test/java/com/sunshine/algorithm/leetcode/competition/week/onehundredeightyone/HasValidPathTest.java
@@ -0,0 +1,28 @@
+package com.sunshine.algorithm.leetcode.competition.week.onehundredeightyone;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author sunshine
+ * @created 2020-03-22
+ */
+@DisplayName("检查网格中是否存在有效路径单元测试")
+public class HasValidPathTest {
+
+	@Test
+	public void hasValidPathTestOne() {
+		Assertions.assertTrue(new HasValidPath().hasValidPathDFS(new int[][]{{2, 4, 3}, {6, 5, 2}}));
+	}
+
+	@Test
+	public void hasValidPathTestTwo() {
+		Assertions.assertFalse(new HasValidPath().hasValidPathDFS(new int[][]{{1, 1, 2}}));
+	}
+
+	@Test
+	public void hasValidPathTestThree() {
+		Assertions.assertTrue(new HasValidPath().hasValidPathDFS(new int[][]{{2}, {2}, {2}, {2}, {2}, {2}, {6}}));
+	}
+}
---
+++ algorithm-leetcode-competition/src/test/java/com/sunshine/algorithm/leetcode/competition/week/onehundredeightyone/SumFourDivisorsTest.java
@@ -0,0 +1,23 @@
+package com.sunshine.algorithm.leetcode.competition.week.onehundredeightyone;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author sunshine
+ * @created 2020-03-22
+ */
+@DisplayName("四因数单元测试")
+public class SumFourDivisorsTest {
+
+	@Test
+	public void sumFourDivisorsTestOne() {
+		Assertions.assertEquals(32, new SumFourDivisors().sumFourDivisors(new int[]{21, 4, 7}));
+	}
+
+	@Test
+	public void sumFourDivisorsTestTwo() {
+		Assertions.assertEquals(64, new SumFourDivisors().sumFourDivisors(new int[]{21, 21}));
+	}
+}
