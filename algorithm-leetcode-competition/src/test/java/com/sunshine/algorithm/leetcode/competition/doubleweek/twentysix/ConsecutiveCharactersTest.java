d4de4f78ccd775d579e516b52286b4d13211f683
SundialDreams <mysunshinedreams@163.com>
2020-05-25 01:07:04 +0800

    [feature]: LeetCode 第26场双周赛

---
+++ algorithm-leetcode-competition/src/test/java/com/sunshine/algorithm/leetcode/competition/doubleweek/twentysix/ConsecutiveCharactersTest.java
@@ -0,0 +1,38 @@
+package com.sunshine.algorithm.leetcode.competition.doubleweek.twentysix;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author sunshine
+ * @created 2020-05-25
+ */
+@DisplayName("连续字符单元测试")
+public class ConsecutiveCharactersTest {
+
+	@Test
+	public void maxPowerTestOne() {
+		Assertions.assertEquals(2, new ConsecutiveCharacters().maxPower("leetcode"));
+	}
+
+	@Test
+	public void maxPowerTestTwo() {
+		Assertions.assertEquals(5, new ConsecutiveCharacters().maxPower("abbcccddddeeeeedcba"));
+	}
+
+	@Test
+	public void maxPowerTestThree() {
+		Assertions.assertEquals(5, new ConsecutiveCharacters().maxPower("triplepillooooow"));
+	}
+
+	@Test
+	public void maxPowerTestFour() {
+		Assertions.assertEquals(11, new ConsecutiveCharacters().maxPower("hooraaaaaaaaaaay"));
+	}
+
+	@Test
+	public void maxPowerTestFive() {
+		Assertions.assertEquals(1, new ConsecutiveCharacters().maxPower("tourist"));
+	}
+}
---
+++ algorithm-leetcode-competition/src/test/java/com/sunshine/algorithm/leetcode/competition/doubleweek/twentysix/SimplifiedFractionsTest.java
@@ -0,0 +1,35 @@
+package com.sunshine.algorithm.leetcode.competition.doubleweek.twentysix;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+/**
+ * @author sunshine
+ * @created 2020-05-25
+ */
+@DisplayName("最简分数单元测试")
+public class SimplifiedFractionsTest {
+
+	@Test
+	public void simplifiedFractionsTestOne() {
+		Assertions.assertIterableEquals(List.of("1/2"), new SimplifiedFractions().simplifiedFractions(2));
+	}
+
+	@Test
+	public void simplifiedFractionsTestTwo() {
+		Assertions.assertIterableEquals(List.of("1/2", "1/3", "2/3"), new SimplifiedFractions().simplifiedFractions(3));
+	}
+
+	@Test
+	public void simplifiedFractionsTestThree() {
+		Assertions.assertIterableEquals(List.of("1/2", "1/3", "2/3", "1/4", "3/4"), new SimplifiedFractions().simplifiedFractions(4));
+	}
+
+	@Test
+	public void simplifiedFractionsTestFour() {
+		Assertions.assertIterableEquals(List.of(), new SimplifiedFractions().simplifiedFractions(1));
+	}
+}
