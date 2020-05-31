688f571fa3080c021730b812ecab638d41b85ec4
SundialDreams <mysunshinedreams@163.com>
2020-05-31 20:17:14 +0800

    [feature]: LeetCode 第191场周赛

---
+++ algorithm-leetcode-competition/src/test/java/com/sunshine/algorithm/leetcode/competition/week/onehundredninetyone/ReorderRoutesToMakeAllPathsLeadToTheCityZeroTest.java
@@ -0,0 +1,31 @@
+package com.sunshine.algorithm.leetcode.competition.week.onehundredninetyone;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author sunshine
+ * @created 2020-05-31
+ */
+@DisplayName("重新规划路线单元测试")
+public class ReorderRoutesToMakeAllPathsLeadToTheCityZeroTest {
+
+	@Test
+	public void minReorderTestOne() {
+		int[][] connections = {{0, 1}, {1, 3}, {2, 3}, {4, 0}, {4, 5}};
+		Assertions.assertEquals(6, new ReorderRoutesToMakeAllPathsLeadToTheCityZero().minReorder(6, connections));
+	}
+
+	@Test
+	public void minReorderTestTwo() {
+		int[][] connections = {{1, 0}, {1, 2}, {3, 2}, {3, 4}};
+		Assertions.assertEquals(2, new ReorderRoutesToMakeAllPathsLeadToTheCityZero().minReorder(5, connections));
+	}
+
+	@Test
+	public void minReorderTestThree() {
+		int[][] connections = {{1, 0}, {2, 0}};
+		Assertions.assertEquals(0, new ReorderRoutesToMakeAllPathsLeadToTheCityZero().minReorder(3, connections));
+	}
+}
