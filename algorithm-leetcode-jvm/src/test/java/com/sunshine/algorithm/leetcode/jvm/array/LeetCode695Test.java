3700f349206faebe3a2eba29561e57f13eed3bb8
慕勋 <muxunpro@gmail.com>
2024-02-16 00:26:42 +0800

    updated: LeetCode695

--- algorithm-leetcode-jvm/src/test/java/com/sunshine/algorithm/leetcode/jvm/array/LeetCode695Test.java
+++ algorithm-leetcode-jvm/src/test/java/com/sunshine/algorithm/leetcode/jvm/array/LeetCode695Test.java
@@ -5,11 +5,11 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 /**
- * @author sunshine
+ * @author 慕勋
  * @created 2020-03-15
  */
-@DisplayName("岛屿的最大面积单元测试")
-public class MaxAreaOfIslandTest {
+@DisplayName("岛屿的最大面积测试用例")
+public class LeetCode695Test {
 
 	@Test
 	public void maxAreaOfIslandTestOne() {
@@ -23,14 +23,16 @@ public class MaxAreaOfIslandTest {
 				{0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0},
 				{0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0}
 		};
-		Assertions.assertEquals(6, new MaxAreaOfIsland().maxAreaOfIsland(matrix));
+		int res = new LeetCode695().maxAreaOfIsland(matrix);
+		int expected = 6;
+		Assertions.assertEquals(expected, res);
 	}
 
 	@Test
 	public void maxAreaOfIslandTestTwo() {
-		int[][] matrix = {
-				{0, 0, 0, 0, 0, 0, 0, 0},
-		};
-		Assertions.assertEquals(0, new MaxAreaOfIsland().maxAreaOfIsland(matrix));
+		int[][] matrix = {{0, 0, 0, 0, 0, 0, 0, 0}};
+		int res = new LeetCode695().maxAreaOfIsland(matrix);
+		int expected = 0;
+		Assertions.assertEquals(expected, res);
 	}
 }
