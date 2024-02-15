9c2fc8604d27599bd6d3405287c361b6e8794b1d
慕勋 <muxunpro@gmail.com>
2024-02-15 23:39:54 +0800

    updated: LeetCode15

--- algorithm-leetcode-jvm/src/main/java/com/sunshine/algorithm/leetcode/jvm/array/LeetCode15.java
+++ algorithm-leetcode-jvm/src/main/java/com/sunshine/algorithm/leetcode/jvm/array/LeetCode15.java
@@ -7,15 +7,18 @@ import java.util.List;
 
 /**
  * 题号：15
- * 题目：三数之和
- * 题链：https://leetcode-cn.com/problems/3sum/
- * 详情：给你一个包含 n 个整数的数组 nums
- * 判断 nums 中是否存在三个元素 a，b，c ，使得 a + b + c = 0 ？
- * 请你找出所有满足条件且不重复的三元组。
- * @author sunshine
+ * <p>
+ * 题目：<a href="https://leetcode-cn.com/problems/3sum/">三数之和</a>
+ * <p>
+ * 详情：给你一个整数数组 nums ，判断是否存在三元组 [nums[i], nums[j], nums[k]] 满足 i != j、i != k 且 j != k ，同时还满足 nums[i] + nums[j] + nums[k] == 0 。
+ * <p>
+ * 请你返回所有和为 0 且不重复的三元组。
+ * <p>
+ * 注意：答案中不可以包含重复的三元组。
+ * @author 慕勋
  * @date 2020/1/20
  */
-public class ThreeSum {
+public class LeetCode15 {
 
 	public List<List<Integer>> threeSum(int[] nums) {
 		if (nums == null || nums.length <= 2) {
--- algorithm-leetcode-jvm/src/test/java/com/sunshine/algorithm/leetcode/jvm/array/LeetCode15Test.java
+++ algorithm-leetcode-jvm/src/test/java/com/sunshine/algorithm/leetcode/jvm/array/LeetCode15Test.java
@@ -8,31 +8,31 @@ import org.junit.jupiter.api.Test;
  * @date 2020/1/20
  */
 @DisplayName("三数之和测试类")
-public class ThreeSumTest {
+public class LeetCode15Test {
 
 	private int[] nums;
 
 	@Test
 	public void threeSumTestOne() {
 		nums = new int[]{-1, 0, 1, 2, -1, -4};
-		new ThreeSum().threeSum(nums).forEach(System.out::println);
+		new LeetCode15().threeSum(nums).forEach(System.out::println);
 	}
 
 	@Test
 	public void threeSumTestTwo() {
 		nums = new int[]{-2, 0, 0, 2, 2};
-		new ThreeSum().threeSum(nums).forEach(System.out::println);
+		new LeetCode15().threeSum(nums).forEach(System.out::println);
 	}
 
 	@Test
 	public void threeSumTestThree() {
 		nums = new int[]{-1, 0, 1, 2, -1, -4};
-		new ThreeSum().threeSum(nums).forEach(System.out::println);
+		new LeetCode15().threeSum(nums).forEach(System.out::println);
 	}
 
 	@Test
 	public void threeSumTestFour() {
 		nums = new int[]{0, -4, -1, -4, -2, -3, 2};
-		new ThreeSum().threeSum(nums).forEach(System.out::println);
+		new LeetCode15().threeSum(nums).forEach(System.out::println);
 	}
 }
