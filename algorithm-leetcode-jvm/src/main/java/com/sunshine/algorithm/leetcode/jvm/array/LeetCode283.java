383d946bdcbd2898743cca26f2d98bb544016b66
慕勋 <muxunpro@gmail.com>
2024-02-15 22:45:43 +0800

    updated: LeetCode283

--- algorithm-leetcode-jvm/src/main/java/com/sunshine/algorithm/leetcode/jvm/array/LeetCode283.java
+++ algorithm-leetcode-jvm/src/main/java/com/sunshine/algorithm/leetcode/jvm/array/LeetCode283.java
@@ -2,12 +2,14 @@ package com.sunshine.algorithm.leetcode.jvm.array;
 
 /**
  * 题号：283
- * 题目：移动零
+ * <p>
+ * 题目：<a href="https://leetcode.cn/problems/move-zeroes">移动零</a>
+ * <p>
  * 详情：给定一个数组 nums，编写一个函数将所有 0 移动到数组的末尾，同时保持非零元素的相对顺序。
- * @author sunshine
+ * @author 慕勋
  * @created 2020-01-29
  */
-public class MoveZeroes {
+public class LeetCode283 {
 
 	/**
 	 * 思路：移位，冒泡排序
@@ -39,10 +41,11 @@ public class MoveZeroes {
 		int lastZeroIndex = 0;
 		for (int i = 0; i < nums.length; i++) {
 			if (nums[i] != 0) {
+				// 非零元素覆盖数组的前lastZeroIndex个索引
 				nums[lastZeroIndex++] = nums[i];
 			}
 		}
-
+		// 之后的部分全用0进行填充
 		for (int i = lastZeroIndex; i < nums.length; i++) {
 			nums[i] = 0;
 		}
--- algorithm-leetcode-jvm/src/test/java/com/sunshine/algorithm/leetcode/jvm/array/LeetCode283Test.java
+++ algorithm-leetcode-jvm/src/test/java/com/sunshine/algorithm/leetcode/jvm/array/LeetCode283Test.java
@@ -9,61 +9,61 @@ import org.junit.jupiter.api.Test;
  * @created 2020-01-29
  */
 @DisplayName("移动零测试")
-public class MoveZeroesTest {
+public class LeetCode283Test {
 
 	@Test
 	public void moveZeroesTestOne() {
 		int[] nums = new int[]{0, 1, 0, 3, 12};
-		new MoveZeroes().moveZeroes(nums);
+		new LeetCode283().moveZeroes(nums);
 		Assertions.assertArrayEquals(new int[]{1, 3, 12, 0, 0}, nums);
 	}
 
 	@Test
 	public void moveZeroesTestTwo() {
 		int[] nums = new int[]{0, 0, 1};
-		new MoveZeroes().moveZeroes(nums);
+		new LeetCode283().moveZeroes(nums);
 		Assertions.assertArrayEquals(new int[]{1, 0, 0}, nums);
 	}
 
 	@Test
 	public void moveZeroesTestThree() {
 		int[] nums = new int[]{0, 0};
-		new MoveZeroes().moveZeroes(nums);
+		new LeetCode283().moveZeroes(nums);
 		Assertions.assertArrayEquals(new int[]{0, 0}, nums);
 	}
 
 	@Test
 	public void moveZeroesTestFour() {
 		int[] nums = new int[]{1, 0, 1};
-		new MoveZeroes().moveZeroes(nums);
+		new LeetCode283().moveZeroes(nums);
 		Assertions.assertArrayEquals(new int[]{1, 1, 0}, nums);
 	}
 
 	@Test
 	public void moveZeroesOptimizationTestOne() {
 		int[] nums = new int[]{0, 1, 0, 3, 12};
-		new MoveZeroes().moveZeroes(nums);
+		new LeetCode283().moveZeroes(nums);
 		Assertions.assertArrayEquals(new int[]{1, 3, 12, 0, 0}, nums);
 	}
 
 	@Test
 	public void moveZeroesOptimizationTestTwo() {
 		int[] nums = new int[]{0, 0, 1};
-		new MoveZeroes().moveZeroes(nums);
+		new LeetCode283().moveZeroes(nums);
 		Assertions.assertArrayEquals(new int[]{1, 0, 0}, nums);
 	}
 
 	@Test
 	public void moveZeroesOptimizationTestThree() {
 		int[] nums = new int[]{0, 0};
-		new MoveZeroes().moveZeroes(nums);
+		new LeetCode283().moveZeroes(nums);
 		Assertions.assertArrayEquals(new int[]{0, 0}, nums);
 	}
 
 	@Test
 	public void moveZeroesOptimizationTestFour() {
 		int[] nums = new int[]{1, 0, 1};
-		new MoveZeroes().moveZeroes(nums);
+		new LeetCode283().moveZeroes(nums);
 		Assertions.assertArrayEquals(new int[]{1, 1, 0}, nums);
 	}
 }
