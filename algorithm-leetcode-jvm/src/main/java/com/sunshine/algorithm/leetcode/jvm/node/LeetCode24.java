e1ca065926a90e45b27c1b8d7cc181d6b53afb2e
慕勋 <muxunpro@gmail.com>
2024-03-06 00:21:08 +0800

    updated: LeetCode24

--- algorithm-leetcode-jvm/src/main/java/com/sunshine/algorithm/leetcode/jvm/node/LeetCode24.java
+++ algorithm-leetcode-jvm/src/main/java/com/sunshine/algorithm/leetcode/jvm/node/LeetCode24.java
@@ -2,7 +2,9 @@ package com.sunshine.algorithm.leetcode.jvm.node;
 
 /**
  * 题号：24
+ * <p>
  * 题目：<a href="https://leetcode.cn/problems/swap-nodes-in-pairs">两两交换链表中的节点</a>
+ * <p>
  * 详情：给定一个链表，两两交换其中相邻的节点，并返回交换后的链表。
  * <p>
  * 你不能只是单纯的改变节点内部的值，而是需要实际的进行节点交换。
