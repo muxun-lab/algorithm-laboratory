7994f713a427a1ab32132b2a3157767900a3a35b
慕勋 <muxunpro@gmail.com>
2024-03-10 12:54:31 +0800

    feat: The 388's Weekly Contest

---
+++ algorithm-leetcode-competition/src/test/java/com/sunshine/algorithm/leetcode/competition/week/threehundredeightyeight/LeetCode100251Test.java
@@ -0,0 +1,29 @@
+package com.sunshine.algorithm.leetcode.competition.week.threehundredeightyeight;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author 慕勋
+ * @created 2024-03-10
+ */
+@DisplayName("数组中的最短非公共子字符串测试用例")
+public class LeetCode100251Test {
+
+    @Test
+    public void testOne() {
+        String[] arr = {"cab", "ad", "bad", "c"};
+        String[] expected = {"ab", "", "ba", ""};
+        String[] resArr = new LeetCode100251().shortestSubstrings(arr);
+        Assertions.assertArrayEquals(expected, resArr);
+    }
+
+    @Test
+    public void testTwo() {
+        String[] arr = {"abc", "bcd", "abcd"};
+        String[] expected = {"", "", "abcd"};
+        String[] resArr = new LeetCode100251().shortestSubstrings(arr);
+        Assertions.assertArrayEquals(expected, resArr);
+    }
+}
