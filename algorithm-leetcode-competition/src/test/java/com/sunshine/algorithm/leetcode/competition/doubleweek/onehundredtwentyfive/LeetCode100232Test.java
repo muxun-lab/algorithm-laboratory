0faf2fb73d2d334aa9101a67a8ae1c8fa3206675
慕勋 <muxunpro@gmail.com>
2024-03-15 01:04:37 +0800

    feat: The 125's Double Weekly Contest

---
+++ algorithm-leetcode-competition/src/test/java/com/sunshine/algorithm/leetcode/competition/doubleweek/onehundredtwentyfive/LeetCode100232Test.java
@@ -0,0 +1,31 @@
+package com.sunshine.algorithm.leetcode.competition.doubleweek.onehundredtwentyfive;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author 慕勋
+ * @created 2024-03-02
+ */
+@DisplayName("超过阈值的最少操作数 II测试用例")
+public class LeetCode100232Test {
+
+    @Test
+    public void testOne() {
+        int[] nums = {2, 11, 10, 1, 3};
+        int k = 10;
+        int expected = 2;
+        int res = new LeetCode100232().minOperations(nums, k);
+        Assertions.assertEquals(expected, res);
+    }
+
+    @Test
+    public void testTwo() {
+        int[] nums = {1, 1, 2, 4, 9};
+        int k = 20;
+        int expected = 4;
+        int res = new LeetCode100232().minOperations(nums, k);
+        Assertions.assertEquals(expected, res);
+    }
+}
