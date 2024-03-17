c9886016c4b2e444e371c1ad91afb4a844d2d970
慕勋 <muxunpro@gmail.com>
2024-03-17 12:16:53 +0800

    feat: The 389's Weekly Contest

---
+++ algorithm-leetcode-competition/src/test/java/com/sunshine/algorithm/leetcode/competition/week/threehundredeightynine/LeetCode100248Test.java
@@ -0,0 +1,45 @@
+package com.sunshine.algorithm.leetcode.competition.week.threehundredeightynine;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author 慕勋
+ * @created 2024-03-17
+ */
+@DisplayName("字符串及其反转中是否存在同一子字符串测试用例")
+public class LeetCode100248Test {
+
+    @Test
+    public void testOne() {
+        String s = "ausoee";
+        boolean expected = true;
+        boolean res = new LeetCode100248().isSubstringPresent(s);
+        Assertions.assertEquals(expected, res);
+    }
+
+    @Test
+    public void testTwo() {
+        String s = "leetcode";
+        boolean expected = true;
+        boolean res = new LeetCode100248().isSubstringPresent(s);
+        Assertions.assertEquals(expected, res);
+    }
+
+    @Test
+    public void testThree() {
+        String s = "abcba";
+        boolean expected = true;
+        boolean res = new LeetCode100248().isSubstringPresent(s);
+        Assertions.assertEquals(expected, res);
+    }
+
+    @Test
+    public void testFour() {
+        String s = "abcd";
+        boolean expected = false;
+        boolean res = new LeetCode100248().isSubstringPresent(s);
+        Assertions.assertEquals(expected, res);
+    }
+}
