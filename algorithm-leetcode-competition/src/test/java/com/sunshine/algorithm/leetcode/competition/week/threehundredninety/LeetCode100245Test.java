7ff6989eb3cdbdf9363e9dd0aaa24589bbda1808
慕勋 <muxunpro@gmail.com>
2024-03-30 00:29:17 +0800

    feat: The 390's Weekly Contest

---
+++ algorithm-leetcode-competition/src/test/java/com/sunshine/algorithm/leetcode/competition/week/threehundredninety/LeetCode100245Test.java
@@ -0,0 +1,29 @@
+package com.sunshine.algorithm.leetcode.competition.week.threehundredninety;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author 慕勋
+ * @created 2024-03-24
+ */
+@DisplayName("每个字符最多出现两次的最长子字符串测试用例")
+public class LeetCode100245Test {
+
+    @Test
+    public void testOne() {
+        String s = "bcbbbcba";
+        int expected = 4;
+        int res = new LeetCode100245().maximumLengthSubstring(s);
+        Assertions.assertEquals(expected, res);
+    }
+
+    @Test
+    public void testTwo() {
+        String s = "aaaa";
+        int expected = 2;
+        int res = new LeetCode100245().maximumLengthSubstring(s);
+        Assertions.assertEquals(expected, res);
+    }
+}
---
+++ algorithm-leetcode-jvm/src/main/java/com/sunshine/algorithm/leetcode/jvm/array/LeetCode2549.java
@@ -0,0 +1,26 @@
+package com.sunshine.algorithm.leetcode.jvm.array;
+
+/**
+ * 题号: 2549
+ * <p>
+ * 链接: <a href="https://leetcode.cn/problems/count-distinct-numbers-on-board">统计桌面上的不同数字</a>
+ * <p>
+ * 详情: 给你一个正整数 n ，开始时，它放在桌面上。在 109 天内，每天都要执行下述步骤：
+ * <p>
+ * <li> 对于出现在桌面上的每个数字 x ，找出符合 1 <= i <= n 且满足 x % i == 1 的所有数字 i 。
+ * <li> 然后，将这些数字放在桌面上。
+ * 返回在 109 天之后，出现在桌面上的 不同 整数的数目。
+ * @author 慕勋
+ * @created 2024-03-23
+ */
+public class LeetCode2549 {
+
+    /**
+     * 思路:
+     * @param n 正整数
+     * @return
+     */
+    public int distinctIntegers(int n) {
+        return 0;
+    }
+}
